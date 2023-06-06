8171c4dc8ddefed33b49655eb569936f004b007b
Leonardorossato <leonardorossato21@gmail.com>
2023-06-06 10:09:32 -0300

    update receiver.entity.ts

--- src/receivers/entities/receiver.entity.ts
+++ src/receivers/entities/receiver.entity.ts
@@ -1,1 +1,30 @@
-export class Receiver {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Receiver {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false, unique: true, type: 'varchar', length: 255 })
+  fullName: string;
+
+  @Column({ nullable: false, unique: true, type: 'varchar', length: 255 })
+  email: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 255 })
+  whatsapp: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 14 })
+  cellphone: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 255 })
+  message: string;
+
+  @CreateDateColumn({ type: Date })
+  createdAt?: Date;
+}
